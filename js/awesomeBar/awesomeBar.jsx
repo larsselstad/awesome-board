6acd41125972ac1cc31da6e94deaed259a9db053
Lars Selstad <lars@lase.no>
2015-04-24 08:30:44 +0200

    Awesomebar lager steps dynamisk

--- js/awesomeBar/awesomeBar.jsx
+++ js/awesomeBar/awesomeBar.jsx
@@ -4,7 +4,30 @@ var React = require('react');
 var AwesomeBarStep = require('./awesomeBarStep.jsx');
 
 var AwesomeBar = React.createClass({
+    getInitialState: function () {
+        return { 
+            steps: [
+                {
+                    text: "Steps Bullet 1",
+                    done: true,
+                    user: "Maybe"
+                },
+                {
+                    text: "Steps Bullet 2",
+                    done: "false",
+                    user: "Maybe"
+                }
+            ]
+        };
+    },
+    
     render: function() {
+        var awesomeBarSteps = this.state.steps.map(function(el, i) {
+            return (
+                <AwesomeBarStep number={i + 1} done={el.done} />
+            );     
+        });
+        
         return (
             <ul className="awesome-bar no-bullets">
                 <li>
@@ -12,8 +35,7 @@ var AwesomeBar = React.createClass({
                     <div className="awesome-bar-now color-bg-now"></div>
                 </li>
             
-                <AwesomeBarStep tittel="Step 1" done="true" />
-                <AwesomeBarStep tittel="Step 2" done="false" />
+                {awesomeBarSteps}
                 
                 <li>
                    <h3 class="awesome-bar-li-title text-right color-text-awesome">Awesome</h3>
@@ -27,38 +49,3 @@ var AwesomeBar = React.createClass({
 });
 
 module.exports = AwesomeBar;
-
-/*
-
-<ul class="awesome-bar no-bullets">
-                <li>
-                   <h3 class="awesome-bar-li-title color-text-now">Now</h3>
-                   <div class="awesome-bar-now color-bg-now"></div>
-                </li>
-                <li>
-                   <h3 class="awesome-bar-li-title text-center color-text-step">Step 1</h3>
-                    <svg class="awesome-star-small" xmlns="http://www.w3.org/2000/svg" width="65" height="55" viewBox="0 0 51 48">
-                        <path fill="#00f" stroke="#00f" stroke-width="2" d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"/>
-                    </svg>
-                </li>
-                <li>
-                   <h3 class="awesome-bar-li-title text-center color-text-step">Step 2</h3>
-                    <svg class="awesome-star-small" xmlns="http://www.w3.org/2000/svg" width="65" height="55" viewBox="0 0 51 48">
-                        <path fill="#fff" stroke="#00f" stroke-width="2" d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"/>
-                    </svg>
-                </li>
-                <li>
-                   <h3 class="awesome-bar-li-title text-center color-text-step">Step 3</h3>
-                    <svg class="awesome-star-small" xmlns="http://www.w3.org/2000/svg" width="65" height="55" viewBox="0 0 51 48">
-                        <path fill="#fff" stroke="#00f" stroke-width="2" d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"/>
-                    </svg>
-                </li>
-                <li>
-                   <h3 class="awesome-bar-li-title text-right color-text-awesome">Awesome</h3>
-                    <svg class="awesome-star-small awesome-bar-awesome" xmlns="http://www.w3.org/2000/svg" width="84" height="75" viewBox="0 0 51 48">
-                        <path fill="#0f0" stroke="#0f0" stroke-width="2" d="m25,1 6,17h18l-14,11 5,17-15-10-15,10 5-17-14-11h18z"/>
-                    </svg>
-                </li>
-            </ul>
-
-*/
--- js/awesomeBar/awesomeBarStep.jsx
+++ js/awesomeBar/awesomeBarStep.jsx
@@ -6,11 +6,11 @@ var AwesomeStepStar = require('./awesomeStepStar.jsx')
 var AwesomeBarLi = React.createClass({
     render: function () {
         var done = this.props.done;
-        var tittel = this.props.tittel;
+        var number = this.props.number;
         
         return (
             <li>
-                <h3 className="awesome-bar-li-title text-center color-text-step">{tittel}</h3>
+                <h3 className="awesome-bar-li-title text-center color-text-step">Step {number}</h3>
                 <AwesomeStepStar done={done} />
             </li>
         );
